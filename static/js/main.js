d19c7def2d30b65087b49b93abe847e95c659465
Joost Baaij <joost@spacebabies.nl>
2020-09-18 11:42:01 +0200

    tweak the randoms so it feels more natural

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,14 @@
 (function() {
+    var theBabies = document.querySelectorAll('.navbar-brand img');
+    var luckyIndex = Math.round(Math.random() * theBabies.length)
+    var lucky = theBabies[luckyIndex];
+    var animations = new Array('anim-spin', 'anim-jump');
+
     function pickLucky() {
-        var theBabies = document.querySelectorAll('.navbar-brand img');
-        var luckyIndex = Math.round(Math.random() * theBabies.length)
-        var lucky = theBabies[luckyIndex];
-        var animations = new Array('anim-spin', 'anim-jump');
         lucky.classList.add(animations[Math.floor(Math.random() * animations.length)]);
-        lucky.style.animationDelay = Math.floor(Math.random() * 10 + 4);
+        lucky.style.animationDelay = Math.floor(Math.random() * 12 + 4);
         clearInterval(timer)
-        timer = setInterval(pickLucky, Math.round(Math.random() * 4000) + 000)
+        timer = setInterval(pickLucky, Math.round(Math.random() * 3000) + 1000)
     }
 
     var timer = setInterval(pickLucky, 1000);
